7c4e59e7d9f48ceac62724190172bfce2b538028
Spark <50857240+sparkdoaz@users.noreply.github.com>
2022-08-27 23:26:23 +0800

    Session 4 Create a Custom Image View finished

---
+++ Introducing SwiftUI/Introducing SwiftUI/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Introducing SwiftUI
+//
+//  Created by 黃建程 on 2022/8/27.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: /*@START_MENU_TOKEN@*/7/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
