c7da4786754de61853fe2b514560b6ced08a65a0
Spark <50857240+sparkdoaz@users.noreply.github.com>
2022-08-28 00:32:13 +0800

    Section 6 Compose the Detail View finished

--- Introducing SwiftUI/Introducing SwiftUI/ContentView.swift
+++ Introducing SwiftUI/Introducing SwiftUI/ContentView.swift
@@ -9,19 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-                .foregroundColor(.green)
-            HStack {
-                Text("Joshua Tree Nation Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+        VStack {
+            MapView()
+                .frame(height: 300.0)
+                .ignoresSafeArea(edges: .top)
+            CircleImage()
+                .offset(x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/-130.0/*@END_MENU_TOKEN@*/)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .foregroundColor(.green)
+                HStack {
+                    Text("Joshua Tree Nation Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+
+                Divider()
+
+                Text("About Turtle Rock")
+                    .font(.title2)
+
+                Text("Descriptive Text goes here")
             }
+            .padding()
+            Spacer()
         }
-        .padding()
     }
 }
 
