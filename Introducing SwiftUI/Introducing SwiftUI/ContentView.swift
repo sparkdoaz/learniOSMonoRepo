dea6c1e30d1735c2a9c8651b3667f5204c13264f
Spark <50857240+sparkdoaz@users.noreply.github.com>
2022-08-27 21:17:40 +0800

    Introducing SwiftUI init

---
+++ Introducing SwiftUI/Introducing SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Introducing SwiftUI
+//
+//  Created by 黃建程 on 2022/8/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Introducing SwiftUI/Introducing SwiftUI/Introducing_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Introducing_SwiftUIApp.swift
+//  Introducing SwiftUI
+//
+//  Created by 黃建程 on 2022/8/27.
+//
+
+import SwiftUI
+
+@main
+struct Introducing_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
