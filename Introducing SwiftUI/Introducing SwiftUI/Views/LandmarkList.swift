6d542cf5c331915fda9377979a32717fd7ba55f4
Spark <50857240+sparkdoaz@users.noreply.github.com>
2022-08-28 01:14:15 +0800

    Section 4 Create the List of Landmarks finished

---
+++ Introducing SwiftUI/Introducing SwiftUI/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Introducing SwiftUI
+//
+//  Created by 黃建程 on 2022/8/28.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
